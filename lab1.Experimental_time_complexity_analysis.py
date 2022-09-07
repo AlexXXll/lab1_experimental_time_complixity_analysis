e951dcf0fc2394fb964d6a05da08e8de1f994f3f
Alex Cooper <adk549@gmail.com>
2022-09-07 23:14:38 +0300

    Some Sortings

--- lab1.Experimental_time_complexity_analysis.py
+++ lab1.Experimental_time_complexity_analysis.py
@@ -65,6 +65,152 @@ for i in range(0, runs):
 ts3 = ts3 / runs
 print('ts3 =', ts3)
 
+# bubble_sort
+ts5 = np.zeros(N)
+prod_arr5 = 1
+time_stamp_5 = np.array([datetime.datetime.now().timestamp()])
+def bubble_sort(arr):
+    def swap(i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
+    n = len(arr)
+    swapped = True
+
+    x = -1
+    while swapped:
+        swapped = False
+        x = x + 1
+        for i in range(1, n-x):
+            if arr[i - 1] > arr[i]:
+                swap(i - 1, i)
+                swapped = True
+
+# quick_sort
+def partition(array, begin, end):
+    pivot_idx = begin
+    for i in xrange(begin+1, end+1):
+        if array[i] <= array[begin]:
+            pivot_idx += 1
+            array[i], array[pivot_idx] = array[pivot_idx], array[i]
+    array[pivot_idx], array[begin] = array[begin], array[pivot_idx]
+    return pivot_idx
+
+def quick_sort_recursion(array, begin, end):
+    if begin >= end:
+        return
+    pivot_idx = partition(array, begin, end)
+    quick_sort_recursion(array, begin, pivot_idx-1)
+    quick_sort_recursion(array, pivot_idx+1, end)
+
+def quick_sort(array, begin=0, end=None):
+    if end is None:
+        end = len(array) - 1
+
+    return quick_sort_recursion(array, begin, end)
+
+# timsort
+MIN_MERGE = 32
+
+
+def calcMinRun(n):
+    """Returns the minimum length of a
+    run from 23-64 so that
+    the len(array)/minrun is less than or
+    equal to a power of 2.
+
+    e.g. 1=>1, ..., 63=>63, 64=>32, 65=>33,
+    ..., 127=>64, 128=>32, ...
+    """
+    r = 0
+    while n >= MIN_MERGE:
+        r |= n & 1
+        n >>= 1
+    return n + r
+
+
+# This function sorts array from left index to
+# to right index which is of size atmost RUN
+def insertionSort(arr, left, right):
+    for i in range(left + 1, right + 1):
+        j = i
+        while j > left and arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
+            j -= 1
+
+
+# Merge function merges the sorted runs
+def merge(arr, l, m, r):
+
+    # original array is broken in two parts
+    # left and right array
+    len1, len2 = m - l + 1, r - m
+    left, right = [], []
+    for i in range(0, len1):
+        left.append(arr[l + i])
+    for i in range(0, len2):
+        right.append(arr[m + 1 + i])
+
+    i, j, k = 0, 0, l
+
+    # after comparing, we merge those two array
+    # in larger sub array
+    while i < len1 and j < len2:
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+
+        else:
+            arr[k] = right[j]
+            j += 1
+
+        k += 1
+
+    # Copy remaining elements of left, if any
+    while i < len1:
+        arr[k] = left[i]
+        k += 1
+        i += 1
+
+    # Copy remaining element of right, if any
+    while j < len2:
+        arr[k] = right[j]
+        k += 1
+        j += 1
+
+
+# Iterative Timsort function to sort the
+# array[0...n-1] (similar to merge sort)
+def timSort(arr):
+    n = len(arr)
+    minRun = calcMinRun(n)
+
+    # Sort individual subarrays of size RUN
+    for start in range(0, n, minRun):
+        end = min(start + minRun - 1, n - 1)
+        insertionSort(arr, start, end)
+
+    # Start merging from size RUN (or 32). It will merge
+    # to form size 64, then 128, 256 and so on ....
+    size = minRun
+    while size < n:
+
+        # Pick starting point of left sub array. We
+        # are going to merge arr[left..left+size-1]
+        # and arr[left+size, left+2*size-1]
+        # After every merge, we increase left by 2*size
+        for left in range(0, n, 2 * size):
+
+            # Find ending point of left sub array
+            # mid+1 is starting point of right sub array
+            mid = min(n - 1, left + size - 1)
+            right = min((left + 2 * size - 1), (n - 1))
+
+            # Merge sub array arr[left.....mid] &
+            # arr[mid+1....right]
+            if mid < right:
+                merge(arr, left, mid, right)
+
+        size = 2 * size
 
 # fig, ax = plt.subplots()
 # ax.plot(n, ts1, label='Экспериментальное значение f(v)=1')
